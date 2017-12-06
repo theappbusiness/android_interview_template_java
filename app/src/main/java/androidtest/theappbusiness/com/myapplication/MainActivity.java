742e37d6a08db018213328200977013eeeebd0ac
Robotoaster <swav@theappbusiness.com>
2017-12-06 08:56:59 +0000

    init

---
+++ app/src/main/java/androidtest/theappbusiness/com/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package androidtest.theappbusiness.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
